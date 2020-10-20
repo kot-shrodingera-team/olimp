487e84513d261b827835c7c8304cc5b2e8389e68
kot-shrodingera <kot.shrodingera@gmail.com>
2020-10-20 17:59:05 +0600

    feat(checkCouponLoading/офшор): обработка ошибки "Дождитесь принятия предыдущей ставки перед приёмом новой"

--- src/worker_callbacks/checkStakeStatus.ts
+++ src/worker_callbacks/checkStakeStatus.ts
@@ -1,7 +1,11 @@
-import { log } from '@kot-shrodingera-team/germes-utils';
+import {
+  log,
+  round,
+  stakeInfoString,
+} from '@kot-shrodingera-team/germes-utils';
 import isClone from '../isClone';
 import isCupis from '../isCupis';
-import { updateBalance } from '../stake_info/getBalance';
+import getBalance, { updateBalance } from '../stake_info/getBalance';
 
 const checkStakeStatus = (): boolean => {
   // ЦУПИС
@@ -74,6 +78,38 @@ const checkStakeStatus = (): boolean => {
     log('Cтавка принята', 'green');
     updateBalance();
     return true;
+  }
+  if (
+    /Дождитесь принятия предыдущей ставки перед приёмом новой/i.test(errorText)
+  ) {
+    const currentBalance = getBalance();
+    log(
+      `Баланс до ставки: ${worker.StakeInfo.Balance}. Текущий баланс: ${currentBalance}. Сумма ставки: ${worker.StakeInfo.Summ}`,
+      'steelblue'
+    );
+    const balanceDrop =
+      round(worker.StakeInfo.Balance - getBalance()) === worker.StakeInfo.Summ;
+    if (balanceDrop) {
+      const message =
+        `В Olimp ошибка ставки "Дождитесь принятия предыдущей ставки перед приёмом новой"\n` +
+        `${stakeInfoString()}\n` +
+        `Баланс изменился на сумму ставки` +
+        `Ставка засчитана как принятая. Желательно проверить вручную\n` +
+        `Если ставка НЕ принята сообщить в ТП\n`;
+      worker.Helper.SendInformedMessage(message);
+      log('Баланс изменился на сумму ставки. Считаем ставку принятой', 'green');
+      updateBalance();
+      return true;
+    }
+    const message =
+      `В Olimp ошибка ставки "Дождитесь принятия предыдущей ставки перед приёмом новой"\n` +
+      `${stakeInfoString()}\n` +
+      `Баланс не изменился на сумму ставки` +
+      `Ставка засчитана как НЕ принятая. Желательно проверить вручную\n` +
+      `Если ставка принята сообщить в ТП\n`;
+    worker.Helper.SendInformedMessage(message);
+    log('Cтавка не принята', 'tomato');
+    return false;
   }
   log(`Ставка не принята (${errorText})`, 'tomato');
   return false;
